c659d3e79cc7d05ba63ac800b2f8ec85ad139090
Preston A Arnette <arnettepreston@gmail.com>
2018-08-08 01:41:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
-}
+}https://github.com/learn-co-students/js-functions-lab-bootcamp-prep-000/tree/solution
